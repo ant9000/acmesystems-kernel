026355d3854857eb6b1f92db1b11fd2b5e3cccc4
Antonio Galea <antonio.galea@gmail.com>
2020-05-14 11:40:11 +0200

    update prepare.sh to kernel 4.19.123

--- prepare.sh
+++ prepare.sh
@@ -17,8 +17,8 @@ if [ ! -f gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf.tar.xz ]; then
   wget http://releases.linaro.org/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabihf/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf.tar.xz
 fi
 
-if [ ! -f linux-4.19.80.tar.xz ]; then
-  wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.80.tar.xz
+if [ ! -f linux-4.19.123.tar.xz ]; then
+  wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.123.tar.xz
 fi
 )
 
@@ -37,13 +37,11 @@ if [ ! -d gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf ]; then
 fi
 )
 
-if [ ! -d linux-4.19.80 ]; then
-  tar xvf downloads/linux-4.19.80.tar.xz
-  cd linux-4.19.80
+if [ ! -d linux-4.19.123 ]; then
+  tar xvf downloads/linux-4.19.123.tar.xz
+  cd linux-4.19.123
   patch -p1 < ../patches/linux-4.19.patch
   cd ..
 fi
 
-if [ ! -L linux-src ]; then
-  ln -s linux-4.19.80 linux-src
-fi
+ln -sf linux-4.19.123 linux-src
